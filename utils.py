121443ca9da202ce020ef2c16712934c81e00b8f
vietnamican <vietnamican@gmail.com>
2020-12-23 23:09:18 +0700

    fix bug in ssim loss

--- utils.py
+++ utils.py
@@ -8,7 +8,8 @@ import torch
 from skimage.measure import label
 import scipy.ndimage
 import scipy.ndimage.morphology
-from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
+# from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
+import pytorch_ssim
 
 from config import im_size, epsilon, epsilon_sqr
 
@@ -142,7 +143,8 @@ def alpha_prediction_loss(y_pred, y_true):
     # mask = y_true
     # diff = y_pred - y_true
     # diff = diff
-    return ms_ssim(y_pred, y_true, data_range=1, size_average=True)
+    return pytorch_ssim.ssim(y_pred, y_true)
+    # return ms_ssim(y_pred, y_true, data_range=1, size_average=True)
     # num_pixels = torch.sum(mask)
     # return torch.sum(torch.sqrt(torch.pow(diff, 2) + epsilon_sqr)) / (y_true.numel() + epsilon)
 
