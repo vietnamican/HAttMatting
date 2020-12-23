f1f5382dc3eb47eb61b41c0ccbd8990400cd5641
vietnamican <vietnamican@gmail.com>
2020-12-23 23:43:24 +0700

    update git ms_ssim loss

--- utils.py
+++ utils.py
@@ -8,8 +8,8 @@ import torch
 from skimage.measure import label
 import scipy.ndimage
 import scipy.ndimage.morphology
-# from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
-import pytorch_ssim
+from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
+# import pytorch_ssim
 
 from config import im_size, epsilon, epsilon_sqr
 
@@ -143,7 +143,8 @@ def alpha_prediction_loss(y_pred, y_true):
     # mask = y_true
     # diff = y_pred - y_true
     # diff = diff
-    return -pytorch_ssim.ssim(y_pred, y_true)
+    return 1 - ms_ssim(y_pred, y_true, data_range=1, size_average=True )
+    # return 1 - pytorch_ssim.ssim(y_pred, y_true)
     # return ms_ssim(y_pred, y_true, data_range=1, size_average=True)
     # num_pixels = torch.sum(mask)
     # return torch.sum(torch.sqrt(torch.pow(diff, 2) + epsilon_sqr)) / (y_true.numel() + epsilon)
