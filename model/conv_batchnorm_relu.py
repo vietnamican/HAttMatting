ae85e3a4111409981652e282b90ff0c883020487
vietnamican <vietnamican@gmail.com>
2020-12-29 23:21:41 +0700

    disable batchnorm to perform training in batch 1

--- model/conv_batchnorm_relu.py
+++ model/conv_batchnorm_relu.py
@@ -14,7 +14,7 @@ class ConvBatchnormRelu(nn.Module):
         super(ConvBatchnormRelu, self).__init__()
         conv_mod = nn.Conv2d(*args, **kwargs)
         outplanes = args[1]
-        self.cbr_unit = nn.Sequential(conv_mod, nn.BatchNorm2d(int(outplanes)), nn.ReLU(inplace=True))
+        self.cbr_unit = nn.Sequential(conv_mod, nn.ReLU(inplace=True))
         # with_bn = kwargs['with_bn'] if 'with_bn' in kwargs else False
         # with_relu = kwargs['with_relu'] if 'with_relu' in kwargs else False
         # if with_bn:
