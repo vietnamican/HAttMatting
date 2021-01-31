3675e3b2fbb60b72ed88157940fa17bed48001c0
vietnamican <vietnamican@gmail.com>
2021-01-31 15:42:03 +0700

    add base module

--- test_model.py
+++ test_model.py
@@ -4,7 +4,6 @@ from time import time
 import torch
 import torchvision
 from torchsummary import summary
-# from torchsummaryX import summary
 from torch.utils.data.dataloader import DataLoader
 import torch.nn.functional as F
 import torch.optim as optim
@@ -12,10 +11,5 @@ import torch.optim as optim
 from model import Model
 
 if __name__ == '__main__':
-    model = Model('train_alpha')
-    # summary(model, (3, 322, 322))
-    model.migrate()
-    # inp = torch.zeros((2, 3, 320, 320))
-    # out1, out2 = model(inp)
-    # print(out1.shape, out2.shape)
-    # summary(model, torch.zeros((2, 3, 320, 320)))
+    model = Model()
+    model.remove_prefix_state_dict({}, prefix=0.1)
